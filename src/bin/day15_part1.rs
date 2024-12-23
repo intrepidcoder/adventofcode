1397b51e8ffd233ed141573751b1a7ef5b699a1d
Daniel Moyer <daniel@intrepidcoder.com>
2024-12-23 09:30:03 -0800

    Test answers for Day 15, Part 1

--- src/bin/day15_part1.rs
+++ src/bin/day15_part1.rs
@@ -1,11 +1,10 @@
 use adventofcode::grid::{Grid, Neighbors};
 use std::io::{self, BufRead};
 
-fn main() {
-    let mut grid = Grid::read_from_input();
+fn solve(mut input: impl BufRead) -> usize {
+    let mut grid = Grid::read_from_buf(&mut input);
     let mut moves = String::new();
-    let mut stdin = io::stdin().lock();
-    while let Ok(len) = stdin.read_line(&mut moves) {
+    while let Ok(len) = input.read_line(&mut moves) {
         if len == 0 {
             break;
         }
@@ -53,12 +52,46 @@ fn main() {
         }
     }
 
-    let result: usize = grid
-        .iter()
+    grid.iter()
         .enumerate()
         .filter(|&(_, c)| *c == 'O')
         .map(|(i, _)| grid.coords(i))
         .map(|(r, c)| 100 * r + c)
-        .sum();
-    println!("{result}");
+        .sum()
+}
+
+fn main() {
+    println!("{}", solve(io::stdin().lock()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::solve;
+    use std::{
+        fs::File,
+        io::{self, BufReader},
+    };
+
+    fn open(path: &str) -> io::Result<BufReader<File>> {
+        let file = File::open(path)?;
+        Ok(BufReader::new(file))
+    }
+
+    #[test]
+    fn test_small_example() {
+        let input = open("input/day15/example_small.txt").expect("Missing file");
+        assert_eq!(2028, solve(input));
+    }
+
+    #[test]
+    fn test_large_example() {
+        let input = open("input/day15/example_large.txt").expect("Missing file");
+        assert_eq!(10092, solve(input));
+    }
+
+    #[test]
+    fn test_input() {
+        let input = open("input/day15/input.txt").expect("Missing file");
+        assert_eq!(1476771, solve(input));
+    }
 }
